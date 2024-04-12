ba568a72a01432b1acf500aa4d7de93d94b2af61
Abduraimov <abduraim0vnurtilek26@gmail.com>
2024-04-12 13:57:10 +0600

    update jsons and massivc

---
+++ myCoffeeHouseProject/Modules/DTO/ProductModel.swift
@@ -0,0 +1,17 @@
+//
+//  ProductModel.swift
+//  myCoffeeHouseProject
+//
+//  Created by Nurtilek on 4/12/24.
+//
+
+import Foundation
+
+struct ProductModel: Codable {
+    
+    let productImage: String
+    let productName: String
+    let productDescription: String
+    let productPrice: Int
+    
+}
